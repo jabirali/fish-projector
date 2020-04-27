e0b09438a0539c9fd476f0c7b73a056e62631662
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-04-27 23:15:12 +0200

    Add: Autoactivate venv.

--- conf.d/projector.fish
+++ conf.d/projector.fish
@@ -1,4 +1,10 @@
 #!/usr/bin/env fish
 
+# Abbreviate main projector functions.
 abbr -ga p projector
 abbr -ga v venv
+
+# Autoactivate virtual environments.
+if [ -e "$VIRTUAL_ENV" ]
+	source $VIRTUAL_ENV/bin/activate.fish
+end
