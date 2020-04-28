95ca5e24c829a06dbf1f1923635c53186532d435
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-04-28 20:53:57 +0200

    Clean: Modularized project creation.

---
+++ functions/projector_git.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+function projector_git -d 'Create Git project'
+	# Check that variables are set.
+	if [ ! -d "$projector_dir" ]
+		echo 'You need to set $projector_dir.'
+		return 1
+	end
+	
+	# Check command line arguments.
+	if [ ! -d "$argv" ]
+		echo 'Please to provide a project name.'
+		return 1
+	end
+	
+	# Determine the project name.
+	set -l dir "$projector_dir/$argv"
+	if [ -d "$dir" ]
+		echo "Error: \"$dir\" exists!"
+		return 1
+	end
+	
+	# Create the new project.
+	echo ":: Creating Git project."
+	mkdir -p "$dir" || return 1
+	cd "$dir"       || return 1
+	git init .      || return 1
+end
---
+++ functions/projector_py.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+function projector_py -d 'Create Python project'
+	# Create a Git project.
+	projector_git $argv || return 1
+	
+	# Create a virtualenv.
+	mkdir -p "$HOME/.virtualenvs" || return 1
+	set -l venv "$HOME/.virtualenvs/$name"
+	if [ ! -d "$venv" ]
+		# Create the venv itself.
+		echo ":: Creating virtualenv."
+		python3 -m venv "$venv"
+	end
+	
+	# Activate virtualenv.
+	echo ":: Activating virtualenv."
+	source "$venv/bin/activate.fish"
+	
+	# Setup the virtualenv.
+	if [ -n "$projector_pip" ]
+		echo ":: Setting up virtualenv."
+		pip3 install $projector_pip
+	end
+end
