8002f75565da5a88c23c93ca5c6226b9c38dc524
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-04-29 17:03:43 +0200

    Add: Automatic venv activation on start.

--- conf.d/projector.fish
+++ conf.d/projector.fish
@@ -6,4 +6,6 @@ abbr -ga p projector
 # Autoactivate virtual environments.
 if [ -e "$VIRTUAL_ENV" ]
 	source $VIRTUAL_ENV/bin/activate.fish
+else if [ -d ~/.virtualenvs/(basename (pwd)) ]
+	~/.virtualenvs/(basename (pwd))
 end
