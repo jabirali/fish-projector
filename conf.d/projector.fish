a33531b982d485eb80c09bfe5cd37a6bc91dadb4
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-04-29 17:15:37 +0200

    Fix: Smart venv activation.

--- conf.d/projector.fish
+++ conf.d/projector.fish
@@ -4,8 +4,9 @@
 abbr -ga p projector
 
 # Autoactivate virtual environments.
+set projector (basename (git rev-parse --show-toplevel 2>/dev/null || echo - ))
 if [ -e "$VIRTUAL_ENV" ]
 	source $VIRTUAL_ENV/bin/activate.fish
-else if [ -d ~/.virtualenvs/(basename (pwd)) ]
-	source ~/.virtualenvs/(basename (pwd))/bin/activate.fish
+else if [ -f ~/.virtualenvs/$projector ]
+	source ~/.virtualenvs/$projector
 end
